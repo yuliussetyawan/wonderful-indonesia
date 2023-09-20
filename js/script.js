bea91ab26694d43248841b970276cc379ab849c5
yuliussetyawan <yuliusetyawan@gmail.com>
2023-09-20 20:11:04 +0700

    footer section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-// ********** nav toggle ************
-// select button and links
+const date = document.querySelector('#date')
 const navBtn = document.querySelector('.nav-toggle');
+// ********** nav toggle ************
 const links = document.querySelector('.nav-links')
 navBtn.addEventListener('click', ()=>{
   links.classList.toggle('show-links')
 })
-
+// ********** set date ************
+date.innerHTML = new Date().getFullYear();
